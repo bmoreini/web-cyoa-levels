6d9170f6b53b1184fa7addd088c257abca9bd872
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-13 21:31:31 -0500

    parses array pairs option, target to new arrays

--- rpg-tollbooth.js
+++ rpg-tollbooth.js
@@ -3,8 +3,10 @@
 
 // core variables
 var messages = []; // for a sequence of messages
-var choices = [];
 var answer = null;
+var options = [];
+var targets = [];
+var data = [];
 // story function variables
 var spin = 0;
 
@@ -20,7 +22,6 @@ function checkAnswers(answer) {  // Matches Scenes  - replace with yours
 	}
 }
 
-
 function parseOptions(data){
 	var options = [];
 	for (i=0;i<data.length;i++){
@@ -29,30 +30,42 @@ function parseOptions(data){
 	return options;
 }
 
+function parseTargets(data){
+	var targets = [];
+	for (i=0;i<data.length;i++){
+		targets[i]=data[i].split("|")[1];
+	}
+	return targets;
+}
+
 function tollbooth() {
 	story("You are wandering along a road on a beautiful spring day, with green meadows and gentle rolling hills.  You don’t have a destination and you have plenty of time.\   \n Suddenly a tollbooth appears in front of you. There is a man in the booth waiting to speak to you.\ \nWhat do you do?");
 	data = ["Enter now|l1s2","Sleep on it|l1s3","Tell mom|l1s4"]
 	options = parseOptions(data);
-	answer = setOptions(options);
+	targets = parseTargets(data);
+	answer = setOptions(options,targets);
 }
 
 function wildNightmares() {
 	story("You have wild nightmares.");
 	data = ["Enter now|l1s2","Tell mom|l1s4","[undefined]|l0s0"]
 	options = parseOptions(data);
-	answer = setOptions(options);
+	targets = parseTargets(data);
+	answer = setOptions(options, targets);
 }
 
 function momCantSeeIt() {
 	story("Mom can't see the tollbooth. She sends you to bed.");
 	data = ["Enter now|l1s2","Sleep on it|l1s5","[undefined]|l0s0"]
 	options = parseOptions(data);
-	answer = setOptions(options);
+	targets = parseTargets(data);
+	answer = setOptions(options, targets);
 }
 
 function enterKingdomOfWisdom(){  // FIRST SCENE - WELCOMES PLAYER
 	story("You see mountains and rivers!");
 	data = ["[undefined]|l1s5","[undefined]|l1s6","[undefined]|l1s7"]
 	options = parseOptions(data);
-	answer = setOptions(options);
+	targets = parseTargets(data);
+	answer = setOptions(options, targets);
 }
