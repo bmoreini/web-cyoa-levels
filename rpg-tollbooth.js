74ff870d9c6243b1a34e4db0b1d0ea2e37e92001
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-11-13 20:27:46 -0500

    Uses array pairs and new parseOptions function

--- rpg-tollbooth.js
+++ rpg-tollbooth.js
@@ -11,7 +11,7 @@ var spin = 0;
 function checkAnswers(answer) {  // Matches Scenes  - replace with yours
 	if (answer == "Enter now") {
 		enterKingdomOfWisdom();
-	} 
+	}
 	else if (answer == "Sleep on it") {
 		wildNightmares();
 	}
@@ -20,26 +20,39 @@ function checkAnswers(answer) {  // Matches Scenes  - replace with yours
 	}
 }
 
-function enterKingdomOfWisdom() { 
-  story("You are wandering along a road on a beautiful spring day, with green meadows and gentle rolling hills.  You don’t have a destination and you have plenty of time.\   \n Suddenly a tollbooth appears in front of you. There is a man in the booth waiting to speak to you.\ \nWhat do you do?");
-	choices = ["Enter Now", "Sleep on it", "Tell Mom"];
-	answer = setOptions(choices);
+
+function parseOptions(data){
+	var options = [];
+	for (i=0;i<data.length;i++){
+		options[i]=data[i].split("|")[0];
+	}
+	return options;
 }
+
+function tollbooth() {
+	story("You are wandering along a road on a beautiful spring day, with green meadows and gentle rolling hills.  You don’t have a destination and you have plenty of time.\   \n Suddenly a tollbooth appears in front of you. There is a man in the booth waiting to speak to you.\ \nWhat do you do?");
+	data = ["Enter now|l1s2","Sleep on it|l1s3","Tell mom|l1s4"]
+	options = parseOptions(data);
+	answer = setOptions(options);
+}
+
 function wildNightmares() {
-	story("Undefined");
-	choices = ["Undefined"];
-	answer = setOptions(choices);
+	story("You have wild nightmares.");
+	data = ["Enter now|l1s2","Tell mom|l1s4","[undefined]|l0s0"]
+	options = parseOptions(data);
+	answer = setOptions(options);
 }
 
 function momCantSeeIt() {
-	story("Undefined");
-	choices = ["Undefined"];
-	answer = setOptions(choices);
+	story("Mom can't see the tollbooth. She sends you to bed.");
+	data = ["Enter now|l1s2","Sleep on it|l1s5","[undefined]|l0s0"]
+	options = parseOptions(data);
+	answer = setOptions(options);
 }
 
-function tollbooth(){  // FIRST SCENE - WELCOMES PLAYER
-	var name = prompt("what is your name?");
-	story("You see a tollbooth appear from nowhere. \ \n On the window is a piece of paper\ \n that says \"Enter,  " + name + "\.\" \n What do you do?");
-	choices = ["Enter now", "Sleep on it", "Tell mom"];
-	answer = setOptions(choices);
+function enterKingdomOfWisdom(){  // FIRST SCENE - WELCOMES PLAYER
+	story("You see mountains and rivers!");
+	data = ["[undefined]|l1s5","[undefined]|l1s6","[undefined]|l1s7"]
+	options = parseOptions(data);
+	answer = setOptions(options);
 }
